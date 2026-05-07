f99ed45cdc3ef361575469a9b0f880bae7da98a5
Albina Milshyna <milshynaalbina@gmail.com>
2026-05-07 20:01:13 +0300

    feat(battle): add gameState structure with player, opponent and gameInfo

--- client/src/pages/Battle.jsx
+++ client/src/pages/Battle.jsx
@@ -1,3 +1,53 @@
+import { useState } from "react";
+
 export default function Battle() {
-    return <h1>Battle</h1>;
+  const [gameState, setGameState] = useState({
+    player: {
+      health: 20,
+      hand: [],
+      field: []
+    },
+    opponent: {
+      health: 20,
+      field: []
+    },
+    gameInfo: {
+      turn: "player",
+      timer: 30,
+      round: 1
+    }
+  });
+
+  return (
+    <div>
+      <h2>Battlefield</h2>
+
+      <div>
+        <h4>Opponent HP: {gameState.opponent.health}</h4>
+        <p>Opponent field cards: {gameState.opponent.field.length}</p>
+      </div>
+
+      <hr/>
+
+      <div>
+        <h4>Your HP: {gameState.player.health}</h4>
+
+        <div>
+          <p>Your cards on field: {gameState.player.field.length}</p>
+        </div>
+
+        <div>
+          <p>Your cards in hand: {gameState.player.hand.length}</p>
+        </div>
+
+        <hr/>
+
+        <div>
+          <p>Turn: {gameState.gameInfo.turn}</p>
+          <p>Time: {gameState.gameInfo.timer}s</p>
+          <p>Round: {gameState.gameInfo.round}</p>
+        </div>
+      </div>
+    </div>
+  );
 }
