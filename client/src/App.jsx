aefcce6b972eb99fe348006e95c778b05fcf971a
Albina Milshyna <milshynaalbina@gmail.com>
2026-05-07 18:12:46 +0300

    feat(router): add app routing structure

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,10 +1,23 @@
 import styles from './styles/App.module.css';
 import HomePage from './pages/HomePage.jsx';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Login from "./pages/Login";
+import Register from "./pages/Register";
+import Lobby from "./pages/Lobby";
+import Battle from "./pages/Battle";
 
 export default function App() {
   return (
-    <div className={styles.app}>
-      <HomePage />
-    </div>
+    <BrowserRouter>
+      <div className={styles.app}>
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/register" element={<Register />} />
+          <Route path="/lobby" element={<Lobby />} />
+          <Route path="/battle" element={<Battle />} />
+        </Routes>
+      </div>
+    </BrowserRouter>
   );
 }
